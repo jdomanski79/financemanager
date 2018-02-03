fcadba93a54bf8154cf5b452ed374fd8a0fce0de
JD <jdomanski79@gmail.com>
2018-02-03 23:54:59 +0100

    zmiana sposobu konfiguracji bazy

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ const MongoStore = require('connect-mongo')(session);
 const routes = require('./routes');
 const transactions= require('./routes/transactions');
 const helpers = require('./lib/helpers');
+const config = require('./config')
 
-
-//const config  = require('./config.js'); 
 
 const app = express();
 
@@ -22,7 +21,7 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGO_URI);// || config.mongoURI);
+mongoose.connect(config.mongoURI);
 
 const db = mongoose.connection;
 mongoose.set('debug', true);
