5e52805c4c184ef28317c02d175aa18b61d8ee15
Jarek Domański <31143961+jdomanski79@users.noreply.github.com>
2018-02-06 08:02:47 +0000

    drobna poprawka

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -53,14 +53,6 @@ exports.index = (req, res, next) => {
           }
         }
       },
-    {
-      $group:{
-        _id: "$_id.type",
-        categories: {$push: {name: "$_id.name", sum: "$sum"}}
-      }
-    },
-
-       
       (err, found) => {
         //found = found.toObject();
         //console.log('Callback!', found);
