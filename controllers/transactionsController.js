9dd827b05f3324ae050972a5af36056cfa5e5a61
Glitch (fluff-word) <none>
2018-02-07 13:12:26 +0000

    trasa do zmiany bazy (zl -> grosze), (transaction Date -> date) zmiany w wyswietlaniu waluty

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -20,18 +20,7 @@ exports.index = (req, res, next) => {
   },
   (err, found) => {
       //found = found.toObject();
-<<<<<<< HEAD
-      console.log('Callback!', found);
-      if (err) return next(err);
-      res.locals.bilans = found.incomes[0].total - found.outcomes[0].total;
-      res.locals.incomes = {
-        total: found.incomes[0].total,
-        categories: found.incomes[0].categories
-      };
-      res.locals.outcomes = {
-        total: found.outcomes[0].total,
-=======
-      if (err) return next(err);
+     if (err) return next(err);
       res.locals.bilans = found.incomes[0].total - found.outcomes[0].total;
       res.locals.incomes = {
         total: found.incomes[0].total.toFixed(2),
@@ -39,7 +28,6 @@ exports.index = (req, res, next) => {
       };
       res.locals.outcomes = {
         total: found.outcomes[0].total.toFixed(2),
->>>>>>> glitch
         categories: found.outcomes[0].categories
       }
       res.render("home", {title: "Strona domowa"});
@@ -156,8 +144,4 @@ function getCategoriesByType (date, categoryType) {
                 }
               }
   )
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> glitch
